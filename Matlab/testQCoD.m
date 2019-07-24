05b575cdc4d934df695b90aacb5309ef9c07f5fb
Shannon Burns <smburns47@ucla.edu>
2019-07-24 13:40:14 -0700

    update noise detection method in testQCoD

--- Matlab/testQCoD.m
+++ Matlab/testQCoD.m
@@ -26,8 +26,8 @@ function [channelmask, QCoDvector] = testQCoD(QCoDthresh, suppressPlot)
         [psd1,~]=pwelch(meand,[],[],[],samprate);
         psdquarters=round(length(psd1)/4);
         Q1 = sum(psd1(1:psdquarters));
-        Q3 = sum(psd1(2*psdquarters+1:3*psdquarters));
-        QCoD = (Q1-Q3)/(Q1+Q3);
+        Q4 = sum(psd1(3*psdquarters+1:end));
+        QCoD = (Q1-Q4)/(Q1+Q4);
         QCoDvector(1,c)=QCoD;
          if QCoD<QCoDthresh
              channelmask(1,c)=0;
@@ -35,8 +35,8 @@ function [channelmask, QCoDvector] = testQCoD(QCoDthresh, suppressPlot)
         meand=d(:,c+numchannels)-mean(d(:,c+numchannels));
         [psd2,fs]=pwelch(meand,[],[],[],samprate);
         Q1 = sum(psd2(1:psdquarters));
-        Q3 = sum(psd2(2*psdquarters+1:3*psdquarters));
-        QCoD = (Q1-Q3)/(Q1+Q3);
+        Q4 = sum(psd2(3*psdquarters+1:end));
+        QCoD = (Q1-Q4)/(Q1+Q4);
         QCoDvector(2,c)=QCoD;
         if QCoD<QCoDthresh
             channelmask(1,c)=0;
