001e097e7eca2671eb50419aafc6d84eddb81220
Shannon Burns <smburns47@ucla.edu>
2020-05-03 22:43:59 -0700

    add MNI calculation for NSP2 data

--- Matlab/getMNIcoords.m
+++ Matlab/getMNIcoords.m
@@ -1,23 +1,23 @@
 function mni_ch_table = getMNIcoords(digfile, SD)
 
 %MNI coordinates of 10-20 reference points
-mni_refs = [61.9958,-15.6227,-34.411;   %RPA
-            2.32,62.34,-21.85;          %Nz
-            0.882,-13.4729,74.1371;     %Cz
-            -60.6623,-15.935,-34.9223;  %LPA
-            -4.77,-100.26,-14.39];      %Iz
+mni_refs = [2.32,62.34,-21.85,1;          %Nz
+            0.882,-13.4729,74.1371,1;     %Cz
+            -60.6623,-15.935,-34.9223,1;  %LPA
+            -4.77,-100.26,-14.39,1];      %Iz
 
 numchannels = size(SD.MeasList,1)/2;
 nSrc = SD.nSrcs;
 nDet = SD.nDets;
 src_det_pair = SD.MeasList(1:numchannels,1:2);
 digpts = readtable(digfile);
-digpts = table2array(digpts(:,2:4));
-digpts_refs = digpts(end-4:end,:);
+digpts = [table2array(digpts(:,2:4)), ones(size(digpts,1),1)];
+digpts_refs = digpts(end-3:end,:);
 
-trsfm = mni_refs' * pinv(digpts_refs');
+trsfm = mni_refs' * pinv(digpts_refs)';
 mni_pts = trsfm * digpts';
 mni_pts = mni_pts';
+mni_pts = mni_pts(:,1:3);
 
 mni_srcPos = mni_pts(1:nSrc,:);
 mni_detPos = mni_pts(nSrc+1:end-5,:);
